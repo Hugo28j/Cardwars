851c156fb76feead307679bcaa61310ce8689c30
Hugo28j <hugo.mendesdacosta@gmail.com>
2026-09-20 23:02:17 +0200

    Refresh map for Reggio Calabria and Catanzaro

--- src/map.js
+++ src/map.js
@@ -1,4 +1,4 @@
-import {CITIES_1300 as CITIES,CITY_1300 as CITY} from './data1300.js?v=20260920-aquileia-venice-coast-fix-v1';
+import {CITIES_1300 as CITIES,CITY_1300 as CITY} from './data1300.js?v=20260920-add-reggio-catanzaro-cards-v1';
 import {icon} from './icons.js';
 const esc=s=>String(s).replace(/[&<>"']/g,c=>({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'}[c]));
 const CITY_DISPLAY_NAMES=new Map([
