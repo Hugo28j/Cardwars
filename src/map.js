36ae5c09b7453ea4f6f2d21cb3d3b7b7c5733237
Hugo28j <hugo.mendesdacosta@gmail.com>
2026-09-20 22:29:25 +0200

    Force fresh single-name map data

--- src/map.js
+++ src/map.js
@@ -1,4 +1,4 @@
-import {CITIES_1300 as CITIES,CITY_1300 as CITY} from './data1300.js?v=20260920-force-single-city-labels-v2';
+import {CITIES_1300 as CITIES,CITY_1300 as CITY} from './data1300.js?v=20260920-force-single-city-labels-v3';
 import {icon} from './icons.js';
 const esc=s=>String(s).replace(/[&<>"']/g,c=>({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'}[c]));
 const CITY_DISPLAY_NAMES=new Map([
