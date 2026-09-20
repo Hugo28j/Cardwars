ca7d09b73b45d563f19ee1061133187784fd7827
Hugo28j <hugo.mendesdacosta@gmail.com>
2026-09-20 21:00:50 +0200

    Bump map border cleanup cache

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import {CITIES,CITY,RARITIES,RARITY_COLORS,DUPLICATE_COINS,PACK} from './data.js
 import {CITIES_1300,CITY_1300,RARITIES_1300,RARITY_COLORS_1300,RESEARCH_1300_NOTE} from './data1300.js?v=20260920-avignon-narbonne-nimes-palma-v1';
 import {freshProfile,formatNumber,openPack,validateProfile} from './engine.js';
 import {icon} from './icons.js';
-import {WorldMap} from './map.js?v=20260920-france-city-territories-v1';
+import {WorldMap} from './map.js?v=20260920-france-border-cleanup-v2';
 const $=s=>document.querySelector(s),app=$('#app'),modal=$('#modal'),KEY='cardwars.collection.v2';
 const esc=s=>String(s??'').replace(/[&<>"']/g,c=>({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'}[c]));
 let profile=freshProfile(),storageFailed=false;
