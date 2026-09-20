bfb9fedcf444aa62f0190831db9e035463e57233
Hugo28j <hugo.mendesdacosta@gmail.com>
2026-09-20 20:06:22 +0200

    Refresh app data for new Burgundy cards

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import {CITIES,CITY,RARITIES,RARITY_COLORS,DUPLICATE_COINS,PACK} from './data.js';
-import {CITIES_1300,CITY_1300,RARITIES_1300,RARITY_COLORS_1300,RESEARCH_1300_NOTE} from './data1300.js?v=20260920-west-europe-map-sync-v2';
+import {CITIES_1300,CITY_1300,RARITIES_1300,RARITY_COLORS_1300,RESEARCH_1300_NOTE} from './data1300.js?v=20260920-burgundy-three-cities-v1';
 import {freshProfile,formatNumber,openPack,validateProfile} from './engine.js';
 import {icon} from './icons.js';
-import {WorldMap} from './map.js?v=20260920-west-europe-map-sync-v2';
+import {WorldMap} from './map.js?v=20260920-burgundy-three-cities-v1';
 const $=s=>document.querySelector(s),app=$('#app'),modal=$('#modal'),KEY='cardwars.collection.v2';
 const esc=s=>String(s??'').replace(/[&<>"']/g,c=>({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'}[c]));
 let profile=freshProfile(),storageFailed=false;
