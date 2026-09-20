5ba29e3c277b5a1319046d831ece2bd027323dcb
Hugo28j <hugo.mendesdacosta@gmail.com>
2026-09-20 11:10:51 +0200

    Refresh recalibrated 1300 card data

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import {CITIES,CITY,RARITIES,RARITY_COLORS,DUPLICATE_COINS,PACK} from './data.js';
-import {CITIES_1300,CITY_1300,RARITIES_1300,RARITY_COLORS_1300,RESEARCH_1300_NOTE} from './data1300.js?v=20260920-serbia-cities';
+import {CITIES_1300,CITY_1300,RARITIES_1300,RARITY_COLORS_1300,RESEARCH_1300_NOTE} from './data1300.js?v=20260920-new22-recalibrated';
 import {freshProfile,formatNumber,openPack,validateProfile} from './engine.js';
 import {icon} from './icons.js';
-import {WorldMap} from './map.js?v=20260920-serbia-cities';
+import {WorldMap} from './map.js?v=20260920-new22-recalibrated';
 const $=s=>document.querySelector(s),app=$('#app'),modal=$('#modal'),KEY='cardwars.collection.v2';
 const esc=s=>String(s??'').replace(/[&<>"']/g,c=>({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'}[c]));
 let profile=freshProfile(),storageFailed=false;
